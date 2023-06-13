fe9111dc1a8c7352d5445a09fe0fe2176f09937e
Selina Karlin <karlinselina@gmail.com>
2023-06-13 13:57:58 +0200

    isAnswerSelected=false so you cant click on more than one

--- script.js
+++ script.js
@@ -14,24 +14,24 @@ function showQuestion() {
 }
 
 function answer(selection) {
-    let question = questions[currentQuestion];                  
-    //um auf aktuelle frage zuzugreifen
-    let selectedAnswerNumber = selection.slice(-1);             
-    //um den letzten buchstaben der answer_ zu bekommen
-    let selected = document.getElementById(selection);
-    let x = question['right_answer'];
-    // um index der richtigen antwort zu holem
-    let rightAnswerSelector  = `#answer_${x}`;
-    // um die id der richtigen antwort zu generiern
+    if (isAnswerSelected) {    //wenn eine antwort schon gewhlt wurde, finden keine weiteren verfrbungen mehr statt
+        return;
+    }
+
+    let question = questions[currentQuestion];              	//um auf aktuelle frage zuzugreifen
+    let selectedAnswerNumber = selection.slice(-1);             //um den letzten buchstaben der answer_ zu bekommen
+    let index = question['right_answer'];
+    let rightAnswerSelector = `#answer_${index}`;
     let rightAnswerElement = document.querySelector(rightAnswerSelector);
-    // greift auf das element mit der ID der richtigen antwort zu
 
     if (selectedAnswerNumber == question['right_answer']) {     // abgleich mit der richtigen antwort, die auch nur eine zahl ist (daher slice-1)
         console.log('richtig!');
-        selected.classList.add('right');
+        document.getElementById(selection).parentNode.classList.add('right');
     } else {
         console.log('falsch!');
-        selected.classList.add('wrong');
+        document.getElementById(selection).parentNode.classList.add('wrong');
         rightAnswerElement.classList.add('right');
     }
+
+    isAnswerSelected = true;
 }
