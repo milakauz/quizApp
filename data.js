651097a6aa6dfbcdbeec7e32b61272725ec40fd6
Selina Karlin <karlinselina@gmail.com>
2023-06-13 11:50:43 +0200

    added questions and answers

---
+++ data.js
@@ -0,0 +1,50 @@
+let questions = [
+    {
+        'question' : 'Wie wird eine Funktion in JavaScript aufgerufen?',
+        'answer_1' : 'call myFunction();',
+        'answer_2' : 'myFunction();',
+        'answer_3' : 'execute myFunction();',
+        'answer_4' : 'runFunction(myFunction);',
+        'right_answer' : 2
+    }, 
+    {
+        'question' : 'Welche der folgenden Methoden wird verwendet, um den Inhalt eines HTML-Elements mit JavaScript zu ändern?',
+        'answer_1' : 'innerHTML()',
+        'answer_2' : 'innerText()',
+        'answer_3' : 'textContent()',
+        'answer_4' : 'setHTML()',
+        'right_answer' : 1
+    }, 
+    {
+        'question' : 'Welche der folgenden Methoden wird verwendet, um eine Klasse zu einer HTML-Elementliste in JavaScript hinzuzufügen?',
+        'answer_1' : 'addClass()',
+        'answer_2' : 'appendClass()',
+        'answer_3' : 'attachClass()',
+        'answer_4' : 'classList.add()',
+        'right_answer' : 4
+    }, 
+    {
+        'question' : 'Welche CSS-Eigenschaft wird verwendet, um den Abstand zwischen den Buchstaben in einem Text anzupassen?',
+        'answer_1' : 'word-spacing',
+        'answer_2' : 'letter-spacing',
+        'answer_3' : 'line-height',
+        'answer_4' : 'text-indent',
+        'right_answer' : 2
+    }, 
+    {
+        'question' : 'Welche Einheit wird in CSS verwendet, um die Breite eines Elements in Prozent des übergeordneten Elements anzugeben?',
+        'answer_1' : 'px',
+        'answer_2' : 'em',
+        'answer_3' : 'rem', 
+        'answer_4' : '%', 
+        'right_answer' : 4
+    }, 
+    {
+        'question' : 'Wann wurde die erste Zeile Code geschrieben?',
+        'answer_1' : '1945',
+        'answer_2' : '1955',
+        'answer_3' : '1965', 
+        'answer_4' : '1975', 
+        'right_answer' : 1
+    }
+];
